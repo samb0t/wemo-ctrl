e8bf6b47dab0256be4b7667ce5286a18a1161d7c
samb0t <sambottoni@gmail.com>
2022-01-10 20:33:45 -0600

    Add sed parsing for state

--- GetBinaryState.sh
+++ GetBinaryState.sh
@@ -1,1 +1,1 @@
-curl --header "Content-Type: text/xml;charset=UTF-8" --header "SOAPAction:\"urn:Belkin:service:basicevent:1#GetBinaryState\"" --data @GetBinaryState.xml http://192.168.1.46:49155/upnp/control/basicevent1 -v 
+curl --header "Content-Type: text/xml;charset=UTF-8" --header "SOAPAction:\"urn:Belkin:service:basicevent:1#GetBinaryState\"" --data @GetBinaryState.xml http://192.168.1.46:49153/upnp/control/basicevent1 -v | sed -En 's/<BinaryState\>([0|1])<\/BinaryState\>/\1/p'
--- SetBinaryStateOff.sh
+++ SetBinaryStateOff.sh
@@ -1,1 +1,1 @@
-curl --header "Content-Type: text/xml;charset=UTF-8" --header "SOAPAction:\"urn:Belkin:service:basicevent:1#SetBinaryState\"" --data @SetBinaryStateOff.xml http://192.168.1.46:49155/upnp/control/basicevent1 -v 
+curl --header "Content-Type: text/xml;charset=UTF-8" --header "SOAPAction:\"urn:Belkin:service:basicevent:1#SetBinaryState\"" --data @SetBinaryStateOff.xml http://192.168.1.46:49153/upnp/control/basicevent1 -v 

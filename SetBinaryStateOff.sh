df12dd8eeec48125fc69d49e264c21ec98808ef9
samb0t <sambottoni@gmail.com>
2022-01-11 18:59:49 -0600

    reformat curl

--- SetBinaryStateOff.sh
+++ SetBinaryStateOff.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-curl --header "Content-Type: text/xml;charset=UTF-8" --header "SOAPAction:\"urn:Belkin:service:basicevent:1#SetBinaryState\"" --data @SetBinaryStateOff.xml http://192.168.1.46:49153/upnp/control/basicevent1 -v 
+curl --header "Content-Type: text/xml;charset=UTF-8" \
+     --header "SOAPAction:\"urn:Belkin:service:basicevent:1#SetBinaryState\"" \
+     --data @SetBinaryStateOff.xml http://192.168.1.46:49153/upnp/control/basicevent1 -v 
